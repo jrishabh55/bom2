b7a5828dade1a67e4a66f5f9fe42b553b043906b
Rishabh Jain <rishabh@jnexsoft.com>
2018-02-20 14:07:10 +0530

    Test

--- src/secComponents/Layout/Layout.js
+++ src/secComponents/Layout/Layout.js
@@ -6,7 +6,7 @@ import '../main.css';
 export class Layout extends Component {
 
     render() {
-        console.log(process.env.LOCAL);
+        console.log(process.env);
         return (<div className="layout">
             <Container fluid="true">
                     <Header/>
