8891d981eae3bfcf95fe4b1a0227f4c6d709f5c9
Rishabh Jain <rishabh@jnexsoft.com>
2018-02-21 18:53:01 +0530

    push

--- src/Guards/index.js
+++ src/Guards/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+
 export class Guard extends Component {
-    render() {
-        return this.canActive();
-    }
+  render() {
+    return this.canActive();
+  }
 }
 
 export default Guard;
